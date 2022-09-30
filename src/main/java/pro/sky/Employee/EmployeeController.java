d73fb486787ec80ddbcd38c4d2dc0b57260758e4
xmin3 <Xmin.34@gmail.com>
2022-09-30 12:12:06 +0300

    commit 2.8HW

--- src/main/java/pro/sky/Employee/EmployeeController.java
+++ src/main/java/pro/sky/Employee/EmployeeController.java
@@ -17,33 +17,61 @@ public class EmployeeController {
     }
 
     @GetMapping("/add")
-    public Employee add(@RequestParam("firstName") String firstName,@RequestParam("lastName") String lastName) {
-        return employeeServis.add(firstName, lastName);
+    public Employee add(@RequestParam("firstName") String firstName,
+                        @RequestParam("lastName") String lastName,
+                        @RequestParam("department") int department,
+                        @RequestParam("id") int id
+    ) {
+        return employeeServis.add(firstName, lastName, department, id);
     }
 
     @GetMapping("/find")
-    public Employee find(@RequestParam("firstName") String firstName,@RequestParam("lastName") String lastName) {
-        return employeeServis.find(firstName, lastName);
+    public Employee find(@RequestParam("firstName") String firstName,
+                         @RequestParam("lastName") String lastName,
+                         @RequestParam("department") int department,
+                         @RequestParam("id") int id
+    ) {
+        return employeeServis.find(firstName, lastName,department,id );
     }
 
     @GetMapping("/delete")
-    public Employee delete(@RequestParam("firstName") String firstName,@RequestParam("lastName") String lastName) {
-        return employeeServis.delete(firstName, lastName);
+    public Employee delete(@RequestParam("firstName") String firstName,
+                           @RequestParam("lastName") String lastName,
+                           @RequestParam("department") int department,
+                           @RequestParam("id") int id) {
+        return employeeServis.delete(firstName, lastName,department,id);
     }
 
-    @GetMapping("/findAll")
+    @GetMapping("/departments/all")
     public Collection<Employee> findAll() {
         return employeeServis.findAll();
     }
+
+    @GetMapping("/departments/all")
+    public Employee findDepartment(
+                         @RequestParam("department") int department) {
+        return employeeServis.finDepartment(department);
+    }
+
+
+    @GetMapping("/departments/all")
+    public Employee findAllInDepartment(
+                                   @RequestParam("departmentId") int department) {
+        return employeeServis.findAllInDepartment(department);
+    }
+
+
 
     @ExceptionHandler //указали  вызвать этот метод;
     public String stringEmployeeNotFoundException(EmployeeNotFoundException e) {
         return "Сотрудник не найден"; //вызывает в коде
     }
+
     @ExceptionHandler //указали  вызвать этот метод;
     public String EmployeeAlreadyAddedException(EmployeeAlreadyAddedException e) {
         return "Сотрудник уже добавлен"; //вызывает в коде
     }
+
     @ExceptionHandler //указали  вызвать этот метод;
     public String EmployeeStorageIsFullException(EmployeeStorageIsFullException e) {
         return "Список сотрудников заполнен"; //вызывает в коде
--- src/main/java/pro/sky/Employee/EmployeeServis.java
+++ src/main/java/pro/sky/Employee/EmployeeServis.java
@@ -3,16 +3,17 @@ package pro.sky.Employee;
 
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
 import java.util.*;
-import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class EmployeeServis {
     private static final int SIZE = 4;
     private String firstName;
     private String lastName;
-    private final Map<String,Employee> employees; //создает список
+    private Integer department;
+    private int salary;
+    private final Map<Integer,Employee> employees; //создает список
 
 
     public EmployeeServis() {
@@ -20,9 +21,10 @@ public class EmployeeServis {
     }
 
 
-    public Employee add(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName); //создает другой список для проверки
-        if (employees.containsKey(employee.getFullName())) //сравнивает в списках на совпадения
+    public Employee add(String firstName, String lastName, Integer department, int salary) {
+        Employee employee = new Employee(firstName, lastName,department,salary); //создает другой список для проверки
+        if (department < 0 || department >= 6) {throw new IllegalArgumentException("Такого депортамента нет");}
+        if (employees.containsValue(employee.getFullName())) //сравнивает в списках на совпадения
         {
             throw new EmployeeAlreadyAddedException();
         }
@@ -30,13 +32,14 @@ public class EmployeeServis {
         {
             throw new EmployeeStorageIsFullException();
         }
-        employees.put(employee.getFullName(),employee);
+        employees.put(employee.getDepartment(),employee);
         return employee;
     }
 
-    public Employee delete(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        if (employees.containsKey(employee.getFullName())) {
+    public Employee delete(String firstName, String lastName,Integer department, int salary) {
+        Employee employee = new Employee(firstName, lastName,department,salary);
+        if (department < 0 || department >= 6) {throw new IllegalArgumentException("Такого депортамента нет");}
+        if (employees.containsValue(employee.getFullName())) {
            return employees.remove(employee.getFullName());
 
         }
@@ -44,9 +47,34 @@ public class EmployeeServis {
     }
 
 
-    public Employee find(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        if (employees.containsKey(employee.getFullName())) {
+    public Employee find(String firstName, String lastName,Integer department,int salary) {
+        Employee employee = new Employee(firstName, lastName,department,salary);
+        if (employees.containsValue(employee.getFullName())) {
+            return employees.get(employee.getFullName());
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+
+    public Employee findAllInDepartment(Integer department) {
+        Employee employee = new Employee(firstName, lastName,department,salary);
+        if (employees.containsKey(employee.getDepartment())) {
+            return employees.get(employee.getFullName());
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+
+
+    public Employee findInDepartmentMaxSalary(Integer department,int salary) {
+        Employee employee = new Employee(firstName, lastName,department,salary);
+       // employees.forEach(employee ->employee ); как тут делать?)
+
+
+
+
+
+        if (employees.containsKey(employee.getDepartment())) {
             return employees.get(employee.getFullName());
         }
         throw new EmployeeNotFoundException();
